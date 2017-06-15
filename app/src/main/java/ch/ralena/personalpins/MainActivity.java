e5ba12d9bb384232a8e965a9ef604a04cbe7c4ee
chickendude <pokemaster103@gmail.com>
2017-06-15 14:37:38 -0400

    Set up Pins and Boards fragments.

--- app/src/main/java/ch/ralena/personalpins/MainActivity.java
+++ app/src/main/java/ch/ralena/personalpins/MainActivity.java
@@ -2,11 +2,17 @@ package ch.ralena.personalpins;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import ch.ralena.personalpins.fragments.BoardFragment;
+import ch.ralena.personalpins.fragments.PinsFragment;
 
+public class MainActivity extends AppCompatActivity {
 	BottomNavigationView bottomNavigationView;
+
+	FragmentManager fragmentManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +21,31 @@ public class MainActivity extends AppCompatActivity {
 
 		bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
 
+		fragmentManager = getSupportFragmentManager();
+
 		bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-			switch (item.getItemId()) {
-				case R.id.actionPins:
+			switch(item.getItemId()) {
+				case R.id.actionBoard:
+					replaceFragment(new BoardFragment());
 					break;
-				case R.id.actionSettings:
+				case R.id.actionPins:
+					replaceFragment(new PinsFragment());
 					break;
 				case R.id.actionTags:
+					replaceFragment(new BoardFragment());
+					break;
+				case R.id.actionUser:
+					replaceFragment(new BoardFragment());
 					break;
 			}
 			return true;
 		});
+		bottomNavigationView.setSelectedItemId(R.id.actionBoard);
+	}
+
+	private void replaceFragment(Fragment fragment) {
+		fragmentManager.beginTransaction()
+				.replace(R.id.frameContainer, fragment)
+				.commit();
 	}
 }
