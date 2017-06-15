1b1196394e4a3c1f5319ad62ff71a120f7e13bac
chickendude <pokemaster103@gmail.com>
2017-06-15 12:06:54 -0400

    Set up Bottom Navigation menu

--- app/src/main/java/ch/ralena/personalpins/MainActivity.java
+++ app/src/main/java/ch/ralena/personalpins/MainActivity.java
@@ -1,13 +1,30 @@
 package ch.ralena.personalpins;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+	BottomNavigationView bottomNavigationView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+		bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+			switch (item.getItemId()) {
+				case R.id.actionPins:
+					break;
+				case R.id.actionSettings:
+					break;
+				case R.id.actionTags:
+					break;
+			}
+			return true;
+		});
 	}
 }
