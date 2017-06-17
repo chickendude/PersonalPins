cce65f2c3cc2ec24085fa357f31a570066b1c22b
chickendude <pokemaster103@gmail.com>
2017-06-17 19:04:22 -0400

    Added missing icons.

--- app/src/main/java/ch/ralena/personalpins/fragments/NewPinFragment.java
+++ app/src/main/java/ch/ralena/personalpins/fragments/NewPinFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -21,21 +23,32 @@ public class NewPinFragment extends Fragment {
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_new_pin, container, false);
+		// set up toolbar
 		MainActivity mainActivity = (MainActivity) getActivity();
-		mainActivity.showActionBar();	// make sure action bar is fully shown
+		mainActivity.showActionBar();    // make sure action bar is fully shown
 		mainActivity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 		toolbar = mainActivity.getSupportActionBar();
-		toolbar.setDisplayHomeAsUpEnabled(true);
+		if (toolbar != null) {
+			toolbar.setDisplayHomeAsUpEnabled(true);
+		}
+		setHasOptionsMenu(true);
 
+		// load views
+		View view = inflater.inflate(R.layout.fragment_new_pin, container, false);
+
+		// load thumbnail
 		ImageView thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
-
 		String filepath = getArguments().getString(PinsFragment.EXTRA_FILEPATH);
 		Picasso.with(view.getContext())
 				.load(filepath)
 				.into(thumbnail);
 
 		return view;
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.ok, menu);
 	}
 
 	@Override
